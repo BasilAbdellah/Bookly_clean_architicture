504c960bb0995dac8ecef013ab8e539f1169775a
Basil <basil.0522002@gmail.com>
2024-07-14 00:52:38 +0300

    book_entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,10 @@
+class bookEntitiy{
+  final String Image;
+  final String title;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  bookEntitiy({required this.Image, required this.title, required this.authorName, required this.price, required this.rating});
+
+}
