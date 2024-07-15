6cf497efb24076e4cdf5f18be0195a66c13577cd
Basil <basil.0522002@gmail.com>
2024-07-15 19:49:56 +0300

    add Either Class

--- lib/Features/home/repos/HomeRepo.dart
+++ lib/Features/home/repos/HomeRepo.dart
@@ -1,6 +1,8 @@
 import 'package:clean/Features/home/domain/entities/book_entity.dart';
+import 'package:clean/core/Errors/Failure.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo{
-  Future<List<bookEntitiy>> fetchFeaturedBooks();
-  Future<List<bookEntitiy>> fetchNewestBooks();
+  Future<Either<Failure,List<bookEntitiy>>> fetchFeaturedBooks();
+  Future<Either<Failure,List<bookEntitiy>>> fetchNewestBooks();
 }
