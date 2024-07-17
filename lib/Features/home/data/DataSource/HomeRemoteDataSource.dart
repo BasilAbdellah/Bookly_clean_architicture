6c6551031cb87bd953830cd875ba5758bd9e6636
Basil <basil.0522002@gmail.com>
2024-07-17 18:11:33 +0300

    cach featured books

--- lib/Features/home/data/DataSource/HomeRemoteDataSource.dart
+++ lib/Features/home/data/DataSource/HomeRemoteDataSource.dart
@@ -1,33 +1,38 @@
 import 'package:clean/Features/home/data/models/BookModel.dart';
 import 'package:clean/Features/home/domain/entities/book_entity.dart';
+import 'package:clean/constants.dart';
 import 'package:clean/core/utils/ApiService.dart';
+import 'package:clean/core/utils/functions/SaveBooks.dart';
 
-abstract class HomeRemoteDataSource{
+abstract class HomeRemoteDataSource {
   Future<List<bookEntitiy>> fetchFeaturedBooks();
   Future<List<bookEntitiy>> fetchNewestBooks();
 }
-class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
+
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
   ApiService apiService;
   HomeRemoteDataSourceImpl(this.apiService);
   @override
   Future<List<bookEntitiy>> fetchFeaturedBooks() async {
-    var data = await apiService.get(endPoint: "volumes?Filtering=free-ebooks&q=programming");
+    var data = await apiService.get(
+        endPoint: "volumes?Filtering=free-ebooks&q=programming");
     List<bookEntitiy> books = getBooksList(data);
-
+    SaveBooksData(books, KFeaturedBox);
     return books;
-
   }
 
   @override
-  Future<List<bookEntitiy>> fetchNewestBooks() async{
-    var data = await apiService.get(endPoint: "volumes?Filtering=free-ebooks&Sorting=newest&q=programming");
+  Future<List<bookEntitiy>> fetchNewestBooks() async {
+    var data = await apiService.get(
+        endPoint: "volumes?Filtering=free-ebooks&Sorting=newest&q=programming");
     List<bookEntitiy> books = getBooksList(data);
 
-    return books;  }
+    return books;
+  }
 
   List<bookEntitiy> getBooksList(Map<String, dynamic> data) {
     List<bookEntitiy> books = [];
-    for(var bookMap in data["items"]){
+    for (var bookMap in data["items"]) {
       books.add(BookModel.fromJson(bookMap));
     }
     return books;
---
+++ lib/core/utils/functions/SaveBooks.dart
@@ -0,0 +1,7 @@
+import 'package:clean/Features/home/domain/entities/book_entity.dart';
+import 'package:hive/hive.dart';
+
+void SaveBooksData(List<bookEntitiy> books , String BoxName) {
+  var box=Hive.box(BoxName);
+  box.addAll(books);
+}
