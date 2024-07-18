4bcf52d0f436d7328a0aca251aac8934a657a4f3
Basil <basil.0522002@gmail.com>
2024-07-18 10:20:48 +0300

    create featured books states

---
+++ lib/Features/home/presentation/manager/featured_books_cubit/fetch_featured_books_state.dart
@@ -0,0 +1,17 @@
+part of 'fetch_featured_books_cubit.dart';
+
+@immutable
+sealed class FetchFeaturedBooksState {}
+
+final class FetchFeaturedBooksInitial extends FetchFeaturedBooksState {}
+final class FetchFeaturedBooksLoading extends FetchFeaturedBooksState {}
+final class FetchFeaturedBooksFailure extends FetchFeaturedBooksState {
+  final String errMessage;
+
+  FetchFeaturedBooksFailure(this.errMessage);
+}
+ final class FetchFeaturedBooksSuccess extends FetchFeaturedBooksState {
+  final List<bookEntitiy> books;
+
+  FetchFeaturedBooksSuccess(this.books);
+}
