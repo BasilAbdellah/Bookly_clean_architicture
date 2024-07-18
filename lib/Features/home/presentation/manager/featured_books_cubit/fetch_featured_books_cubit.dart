aadd5cc067c91fd4fa86ff8ca2beb17e62fa98af
Basil <basil.0522002@gmail.com>
2024-07-18 10:32:52 +0300

    create featured books cubits

--- lib/Features/home/presentation/manager/featured_books_cubit/fetch_featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/featured_books_cubit/fetch_featured_books_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:clean/Features/home/domain/UseCases/FetchFeaturedBooksUseCase.dart';
 import 'package:clean/Features/home/domain/entities/book_entity.dart';
 import 'package:meta/meta.dart';
 
 part 'fetch_featured_books_state.dart';
 
 class FetchFeaturedBooksCubit extends Cubit<FetchFeaturedBooksState> {
-  FetchFeaturedBooksCubit() : super(FetchFeaturedBooksInitial());
+  FetchFeaturedBooksCubit(this.featuredBooksUseCase) : super(FetchFeaturedBooksInitial());
+final FetchFeaturedBooksUseCase featuredBooksUseCase;
+  Future<void> fetchFeaturedBooks()async{
+    emit(FetchFeaturedBooksLoading());
+    var response = await featuredBooksUseCase.call();
+    response.fold((failure) {
+      emit(FetchFeaturedBooksFailure(failure.message));
+    }, (books) {
+    emit(FetchFeaturedBooksSuccess(books));
+    },);
+}
 }
